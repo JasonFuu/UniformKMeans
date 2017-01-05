c0ecb0611736be1c91e585bcd847a68405df183d
JasonFuu <jasonfu@princeton.edu>
2017-01-05 01:15:58 -0500

    finished alg, need to make code not look like trash

--- Testing.py
+++ Testing.py
@@ -4,33 +4,6 @@ from Equal_Cluster_K_Means import Equal_K_Means
 import matplotlib.pyplot as plt
 import random
 
-"""Tests K-means++ with 50 randomly generated 2-var data points in various ranges"""
-def KMeansPlusPlus_Test():
-    points = []
-    xs = []
-    ys = []
-
-    for a in range(50):
-        a = 100*random.random()
-        b = 50*random.random()
-        xs.append(a)
-        ys.append(b)
-        points.append([a, b])
-
-    test = K_Means_Plus_Plus(points, 7)
-
-    centroidx = []
-    centroidy = []
-
-    for points in test.final_centroids():
-        centroidx.append(points[0])
-        centroidy.append(points[1])
-
-    plt.scatter(xs, ys, color = 'red')
-    plt.scatter(centroidx, centroidy, color = 'black')
-
-    plt.show()
-    print(test.final_centroids())
 
 def main():
     points = []
@@ -85,6 +58,5 @@ def main():
 
     plt.scatter(centroidsx, centroidsy, color = 'black')
     plt.show()
-
 
 main()
